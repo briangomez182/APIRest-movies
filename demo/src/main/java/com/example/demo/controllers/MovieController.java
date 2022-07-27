1a0120ec02edc2b0e98fdc55efc771b03a249fe9
Brian Gomez <briangomezz182@gmail.com>
2022-07-27 17:44:22 -0300

    Add new endpoint like creat movie and find one movie

--- demo/src/main/java/com/example/demo/controllers/MovieController.java
+++ demo/src/main/java/com/example/demo/controllers/MovieController.java
@@ -1,15 +1,12 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.Movie;
-import com.example.demo.repositories.interfaces.IMoviesRepository;
 import com.example.demo.services.IMoviesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Array;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movies")
@@ -19,23 +16,21 @@ public class MovieController {
     private IMoviesService iMoviesService;
 
     // Search movies
-    @GetMapping("/all")
+    @GetMapping("/")
     public List<Movie> create() {
-        System.out.println("nuevo");
-
-        
         return iMoviesService.findAll();
     }
 
-    // Search movies
-    @GetMapping("/test")
-    public String test() {
-        int[] intArray = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
+    // Search a movie
+    @GetMapping("/{id}")
+    public Optional<Movie> searchMovie(@PathVariable("id") Integer idMovie) {
+        return iMoviesService.findById(idMovie);
+    }
 
-        for (int numero: intArray) {
-            System.out.println("el valor " + numero);
-        }
-        return "prueba";
+    // Search a movie
+    @PostMapping("/")
+    public Movie saveMovie(@RequestBody() Movie movie) {
+        return iMoviesService.save(movie);
     }
 }
 
